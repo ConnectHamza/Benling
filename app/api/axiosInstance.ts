693c753976241dc29858b0bacc5e0b060a5c097e
DESKTOP-RNHAL1D\Hamza <hamzaahmedb7n2@gmail.com>
2025-06-11 21:06:19 +0500

    Base  URL Updated

--- app/api/axiosInstance.ts
+++ app/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://10.0.0.80:8040/api/v1/BenlingWeb/web",
+  baseURL: "https://api.crownelectricmobility.com/api/v1/BenlingWeb/web",
   headers: {
     "Content-Type": "application/json-patch+json",
   },
