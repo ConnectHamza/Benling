85cff848d1fe3afda64432af106b1e953151e41b
Umair Dehlvi <Umair.Dehlvi@gmail.com>
2025-06-11 16:29:51 +0500

    add: google analytics & google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,40 @@
 // app/layout.tsx
-import '../styles/globals.css';
-import Header from '@/components/Header/header';
-import Footer from '@/components/Footer/footer';
-import ClientWrapper from '@/utils/clientWrapper';
-import SmoothScroll from '@/utils/smoothScroll';
-import Script from 'next/script';
+import "../styles/globals.css";
+import Header from "@/components/Header/header";
+import Footer from "@/components/Footer/footer";
+import ClientWrapper from "@/utils/clientWrapper";
+import SmoothScroll from "@/utils/smoothScroll";
+import Script from "next/script";
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
-  title: 'Crown Benling Electric Mobility - Electrifying Dreams',
-  description: 'Leading the future of electric mobility with Crown Benling.',
-  metadataBase: new URL('https://crownelectricmobility.com'),
+  title: "Crown Benling Electric Mobility - Electrifying Dreams",
+  description: "Leading the future of electric mobility with Crown Benling.",
+  metadataBase: new URL("https://crownelectricmobility.com"),
   alternates: {
-    canonical: '/',
+    canonical: "/",
   },
   openGraph: {
-    title: 'Crown Benling Electric Mobility - Electrifying Dreams',
-    description: 'Leading the future of electric mobility with Crown Benling.',
-    url: 'https://crownelectricmobility.com/',
-    siteName: 'Crown Electric Mobility',
-    type: 'website',
+    title: "Crown Benling Electric Mobility - Electrifying Dreams",
+    description: "Leading the future of electric mobility with Crown Benling.",
+    url: "https://crownelectricmobility.com/",
+    siteName: "Crown Electric Mobility",
+    type: "website",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-XKWM0HCQX3"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XKWM0HCQX3');
-          `}
-        </Script>
+        <GoogleAnalytics gaId="G-XKWM0HCQX3" />    
+        <GoogleTagManager gtmId="GTM-M552XJC5" /> 
       </head>
-      <body>
+      <body>      
         <Header />
         <SmoothScroll>
           <ClientWrapper>{children}</ClientWrapper>
